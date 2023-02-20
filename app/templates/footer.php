fffecc9fde783b325c6762ab699cb4f781e173c5
PengenNgoding <princelukas22@gmail.com>
2023-02-21 05:08:56 +0700

    first

---
+++ app/templates/footer.php
@@ -0,0 +1,7 @@
+<!-- BEGIN: Footer-->
+  <footer class="footer footer-static footer-light">
+   
+      <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="feather icon-arrow-up"></i></button>
+    </p>
+  </footer>
+  <!-- END: Footer-->
